dbbd506688f252acbdc5a01a08665bcaba0f2c86
newStellar <02nahid02@gmail.com>
2016-01-27 13:52:24 +0600

    realtime user tag suggestion added

--- script.js
+++ script.js
@@ -9,22 +9,39 @@ app.controller("myCtrl",function($scope){
         msg : ""
     };
 
+    $scope.userTag = {
+        enable     : false,
+        startIndex : -1,
+    };
+
     $scope.commentList = [{author: "Nahid hasan prodhan",msg :"hi !!!! , how are you all"},{author: "AKM Rezaul Alam",msg :"hello, friends. !! "},{author: "Arifur Rahman",msg :"ASP.net is fun !!"},{author: "Glostars",msg :"How do u like our pera"}]
-    $scope.friendList = ["Barak obama","Billgates","Shahrukh khan","john smith","will smith"]
+    $scope.friendList = ["Barak obama","Billgates","Shahrukh khan","john smith","will smith"];
+    $scope.tempFriendList = ["Barak obama","Billgates","Shahrukh khan","john smith","will smith"];
     $scope.mobile = "nexus 5";
     $scope.rowSize = 1;
     $scope.editCommentrowSize = 1;
     $scope.message = "";
-    $scope.userTag = false;
+
     var rowLen = 62,tmpMsg;
 
     $scope.testEnter =function(){
         var msg =$scope.message;
-
         console.log(tmpMsg+"__"+msg+"-->"+msg[msg.length-1]+msg.length);
-        if(msg[msg.length-1] == '@')$scope.userTag =true;
 
-        if(tmpMsg!=null && tmpMsg[tmpMsg.length-1] == "@")$scope.userTag =false;
+        if($scope.userTag.enable){
+            var str = msg.substr($scope.userTag.startIndex);
+            $scope.tempFriendList = [];
+            for(var i=0;i<$scope.friendList.length;i++){
+                if($scope.friendList[i].indexOf(str) > -1) $scope.tempFriendList.push($scope.friendList[i]);
+            }
+        }
+        if(msg[msg.length-1] == '@'){
+
+            $scope.userTag.enable =true;
+            $scope.userTag.startIndex = msg.length;
+        }
+        if(tmpMsg!=null && msg[msg.length-2]!="@" &&tmpMsg[tmpMsg.length-1] == "@")$scope.userTag.enable =false;
+
         if(msg.length%rowLen == 0 ) {
             $scope.rowSize = (msg.length/rowLen)+1;
         }
@@ -60,6 +77,6 @@ app.controller("myCtrl",function($scope){
     $scope.getSuggestion =function(index){
 
         $scope.message = tmpMsg.slice(0,-1)+" "+$scope.friendList[index];
-        $scope.userTag =false;
+        $scope.userTag.enable =false;
     }
 });
